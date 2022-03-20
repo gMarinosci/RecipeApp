014c61e08b46e8c0b292f2572d5d32fde8f23dda
Gabriele Marinosci <gm222hj@student.lnu.se>
2022-03-20 19:44:43 +0100

    Initialized classes

---
+++ src/model/entities/Ingredient.java
@@ -0,0 +1,43 @@
+package model.entities;
+
+public class Ingredient {
+
+  private String name;
+  private String unit;
+  private double price;
+
+  //all args constructor
+  public Ingredient(String name, String unit, double price) {
+    this.name = name;
+    this.unit = unit;
+    this.price = price;
+  }
+
+  //no args constructor
+  public Ingredient() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getUnit() {
+    return unit;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setUnit(String unit) {
+    this.unit = unit;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+}
